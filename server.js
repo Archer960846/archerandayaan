347ad7d87c84e553cd28fdeb095f96a77fe232a5
Archer960846 <archer.alvesteffer@gmail.com>
2026-02-15 17:29:57 -0500

    Refactor server.js for cleaner code structure

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server);
 
 app.use(express.static("public"));
 
@@ -31,3 +26,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
